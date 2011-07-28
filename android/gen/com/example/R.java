f44a92cb83eb9b6bc67ba238f572777ae8efe1a7
sunqipeng <qipengsun@139.com>
2011-07-28 09:05:45 +0800

    view的测试没有成功，学习adapterview

--- android/gen/com/example/R.java
+++ android/gen/com/example/R.java
@@ -10,22 +10,26 @@ package com.example;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int line_color=0x7f050000;
+    }
     public static final class drawable {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int btn=0x7f060003;
-        public static final int e=0x7f060004;
-        public static final int endOperation=0x7f060001;
-        public static final int tv=0x7f060002;
-        public static final int tv2=0x7f060000;
+        public static final int appv=0x7f070005;
+        public static final int btn=0x7f070003;
+        public static final int e=0x7f070004;
+        public static final int endOperation=0x7f070001;
+        public static final int tv=0x7f070002;
+        public static final int tv2=0x7f070000;
     }
     public static final class layout {
         public static final int hello=0x7f030000;
         public static final int main=0x7f030001;
     }
     public static final class menu {
-        public static final int menu=0x7f050000;
+        public static final int menu=0x7f060000;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
--- android/src/com/example/AppView.java
+++ android/src/com/example/AppView.java
@@ -1,6 +1,9 @@
 package com.example;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -25,4 +28,19 @@ public class AppView extends View {
         super(context, attrs, defStyle);
     }
 
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);    //To change body of overridden methods use File | Settings | File Templates.
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);    //To change body of overridden methods use File | Settings | File Templates.
+        Paint p = new Paint();
+        p.setColor(R.color.line_color);
+        p.setStrokeCap(Paint.Cap.BUTT);
+        p.setTextAlign(Paint.Align.LEFT);
+        canvas.drawRect(new Rect(20, 20, 50, 50), p);
+
+    }
 }
