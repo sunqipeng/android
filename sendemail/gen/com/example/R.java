f2ab5b1336028bb5a8c6f356e744d1ebc1678c02
sunqipeng <qipengsun@139.com>
2011-08-13 00:35:25 +0800

    需要继续改进

--- sendemail/gen/com/example/R.java
+++ sendemail/gen/com/example/R.java
@@ -17,6 +17,8 @@ public final class R {
     public static final class id {
         public static final int imageButton=0x7f050001;
         public static final int imageButton2=0x7f050002;
+        public static final int imageButton3=0x7f050003;
+        public static final int imageswitch=0x7f050004;
         public static final int tv=0x7f050000;
     }
     public static final class layout {
--- sendemail/src/com/example/MyActivity.java
+++ sendemail/src/com/example/MyActivity.java
@@ -3,8 +3,11 @@ package com.example;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageSwitcher;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MyActivity extends Activity {
@@ -50,6 +53,45 @@ public class MyActivity extends Activity {
                 MyActivity.this.startActivityForResult(i, 999);
             }
         });
+
+        initSelectImage();
+        initImageSwitch();
+    }
+
+    private void initSelectImage() {
+        Button imageButton2 = (Button) this.findViewById(R.id.imageButton3);
+
+        imageButton2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Toast.makeText(MyActivity.this, "Select a attchment", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                Intent i = Intent.createChooser(intent, "Please select a attach ");
+                MyActivity.this.startActivityForResult(i, 999);
+
+            }
+        });
+    }
+
+    /**
+     *
+     */
+    private void initImageSwitch() {
+        ImageSwitcher imageSwitcher = (ImageSwitcher) this.findViewById(R.id.imageswitch);
+        try {
+            if (result != null) {
+                //imageSwitcher.setImageURI(result.getData());
+                ImageView image = new ImageView(this);
+                image.setImageURI(result.getData());
+                if (imageSwitcher.getNextView() == null)
+                    imageSwitcher.addView(image);
+                imageSwitcher.setImageURI(result.getData());
+            }
+
+        } catch (Exception ee) {
+            ee.printStackTrace();
+        }
+
     }
 
     @Override
@@ -58,6 +100,7 @@ public class MyActivity extends Activity {
         if (resultCode == RESULT_OK && requestCode == 999) {
             Toast.makeText(this, "selected a data" + data.getData().toString(), Toast.LENGTH_LONG).show();
             result = data;
+            initImageSwitch();
         } else {
             Toast.makeText(this, "does not select a attachment", Toast.LENGTH_LONG).show();
         }
